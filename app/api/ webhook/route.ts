4dacee899ed27d650a3459c0fa05d44539a1fbb8
saurav <sauravchoudhary@sauravs-MacBook-Air-2.local>
2026-05-24 11:48:55 +0530

    test webhook

--- app/api/ webhook/route.ts
+++ app/api/ webhook/route.ts
@@ -9,38 +9,17 @@ from "@/services/automation";
 
 // VERIFY WEBHOOK
 
-export async function GET(
-  req: Request
-) {
+export async function GET(req: Request) {
 
   const { searchParams } =
     new URL(req.url);
 
-  const mode =
-    searchParams.get("hub.mode");
-
-  const token =
-    searchParams.get("hub.verify_token");
-
   const challenge =
     searchParams.get("hub.challenge");
 
-  if (
-    mode === "subscribe" &&
-   token === process.env.WHATSAPP_VERIFY_TOKEN
-  ) {
-
-    return new Response(challenge, {
-      status: 200,
-    });
-  }
-
-  return new Response(
-    "Verification failed",
-    {
-      status: 403,
-    }
-  );
+  return new Response(challenge || "working", {
+    status: 200,
+  });
 }
 
 
